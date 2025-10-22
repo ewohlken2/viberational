0e562d67f5f3cfab0c1d7b08cbc0f0a349a5e9c0
Elvis Wohlken <ewohlken@gmail.com>
2025-10-22 13:21:18 -0700

    added animation to hero banner

--- src/app/hero.js
+++ src/app/hero.js
@@ -1,8 +1,6 @@
 'use client';
-
-import Link from 'next/link'
-
-
+import "./hero.css";
+import Link from 'next/link';
 import Image from "next/image";
 
 export default function HeroBanner() {
@@ -16,16 +14,18 @@ export default function HeroBanner() {
                     <div className="title">
                         <span className="intro-header">Hello, my name is </span>
                         <h1 className="main-header">Elvis Wohlken</h1>
-                        <div className="intro-header-2">I&apos;m a Full Stack Web Developer in LA and</div>
+                        <div className="intro-header-2">I&apos;m a Full Stack Web Developer in LA </div>
                     </div>
                     <div className="button-container">
+                        <div className="button-inner">
                         <Link className="button button--light" href="/contact">
                             I want to work with you
                         </Link>
+                        </div>
                     </div>
                 </div>
 
-                <picture className="main-banner-image"><Image
+                <picture className="main-banner-image fade-in"><Image
                     className="image"
                     src="/portrait-cutout-1000-redder.png"
                     alt="Hero Portrait of Elvis Wohlken"
